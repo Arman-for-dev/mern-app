1976c5c2e0b43562f2e4fa8e58d6767f516b9585
Arman-for-dev <armanfordev59@gmail.com>
2025-08-09 08:10:43 +0600

    app server setup

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import "dotenv/config";
+
+
+
+const app = express();
+
+
+
+
+
+const PORT = process.env.PORT || 50000;
+app.listen(PORT, ()=>{
+    console.log(`Server is running on ${PORT}`);
+});

0141f1b595f0564e03ca3e5f5ee9a9544a03689a
Arman-for-dev <armanfordev59@gmail.com>
2025-08-09 08:43:53 +0600

    config database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,27 @@
 import express from "express";
 import "dotenv/config";
-
+// import todosList from './routes/todosRoute.js'
+import connectDB from "./config/db.js";
 
 
 const app = express();
 
+//Database connection
+connectDB();
+
+//middleware
+app.use(express.json());
 
 
 
 
-const PORT = process.env.PORT || 50000;
+
+//routes
+// app.use('/api/todos', todosList);
+
+
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>{
     console.log(`Server is running on ${PORT}`);
 });
